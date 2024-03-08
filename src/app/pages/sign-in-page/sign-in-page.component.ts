a17ef359c1a63e55677322f429521f253ba55bca
Alexandru Iordache <iordache.alexandru2002@gmail.com>
2024-03-08 15:46:46 +0200

    #1 - Fixes width problem and start form design

--- src/app/pages/sign-in-page/sign-in-page.component.ts
+++ src/app/pages/sign-in-page/sign-in-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
   styleUrl: './sign-in-page.component.scss'
 })
-export class SignInPageComponent {
+export class SignInPageComponent implements OnInit {
+
   isLogin: boolean = true;
+  loginForm: FormGroup = new FormGroup({});
+  registerForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.registerForm = this.formBuilder.group({
+      profileName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
+
+  onLoginSubmit(): void {
+
+  }
 }
