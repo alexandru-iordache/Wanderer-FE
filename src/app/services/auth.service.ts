a88e44e283083cd43d157949ab33841cc3b870bf
Alexandru Iordache <iordache.alexandru2002@gmail.com>
2024-04-01 19:58:43 +0300

    Finalize authentication on FE

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GoogleAuthProvider, browserLocalPersistence, browserSessionPersistence, getAuth, setPersistence } from 'firebase/auth';
+import firebase from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) {
+  }
+
+  async signUp(email: string, password: string) {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  async signIn(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  async signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return this.auth.signInWithPopup(provider);
+  }
+
+  async signOut() {
+    return this.auth.signOut();
+  }
+
+  getAuthState() {
+    return this.auth.authState;
+  }
+
+  setPersistence(rememberMe: boolean) {
+    setPersistence(getAuth(), rememberMe ? browserLocalPersistence : browserSessionPersistence)
+      .catch((error) => {
+        throw error;
+      })
+  }
+
+  async getIdToken() {
+    return (await this.auth.currentUser)?.getIdToken();
+  }
 }
